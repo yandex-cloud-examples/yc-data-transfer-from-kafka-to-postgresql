fd1837024c116043f821f6efa082caa3f9a460dc
Nadezhda-Subotka <subotka-n@yandex-team.ru>
2024-06-20 17:58:39 +0300

    YCDOCIO-3571: Updated Terraform manifests

--- kafka-postgresql.tf
+++ kafka-postgresql.tf
@@ -1,42 +1,67 @@
-# Infrastructure for Yandex Cloud Managed Service for PostgreSQL cluster and Yandex Cloud Managed Service for Apache Kafka®
+# Infrastructure for Yandex Cloud Managed Service for PostgreSQL and Managed Service for Apache Kafka®
 #
-# RU: https://cloud.yandex.ru/docs/data-transfer/tutorials/mkf-to-mpg
-# EN: https://cloud.yandex.com/en/docs/data-transfer/tutorials/mkf-to-mpg
+# RU: https://yandex.cloud/ru/docs/data-transfer/tutorials/mkf-to-mpg
+# EN: https://yandex.cloud/en/docs/data-transfer/tutorials/mkf-to-mpg
+#
+# Configure the parameters of the source and target clusters:
 
-# Specify the following settings
 locals {
-  pg_version = "" # Set a desired version of PostgreSQL. For available versions, see the documentation main page : https://cloud.yandex.com/en/docs/managed-postgresql/
-  kf_version = "" # Set a desired version of Apache Kafka®. For available versions, see the documentation main page : https://cloud.yandex.com/en/docs/managed-kafka/
-  pg_password = "" # Set a password for the PostgreSQL admin user
-  kf_password = "" # Set a password for the Apache Kafka® user
+  pg_version  = "" # Desired version of PostgreSQL. For available versions, see the documentation main page: https://yandex.cloud/en/docs/managed-postgresql/.
+  pg_password = "" # PostgreSQL admin's password
+  kf_version  = "" # Desired version of Apache Kafka®. For available versions, see the documentation main page: https://yandex.cloud/en/docs/managed-kafka/.
+  kf_password = "" # Apache Kafka® user's password
 
-  # Specify these settings ONLY AFTER the clusters are created. Then run "terraform apply" command again
+  # Specify these settings ONLY AFTER the clusters are created. Then run "terraform apply" command again.
   # You should set up endpoints using the GUI to obtain their IDs
   kf_source_endpoint_id = "" # Set the source endpoint ID
-  transfer_enabled      = 0                      # Set to 1 to enable transfer
+  transfer_enabled      = 0  # Set to 1 to enable the transfer
+
+  # The following settings are predefined. Change them only if necessary.
+
+  # Managed Service for PostgreSQL:
+  mpg_network_name        = "mpg_network"        # Name of the network for the PostgreSQL cluster
+  mpg_subnet_name         = "mpg_subnet-a"       # Name of the subnet for the PostgreSQL cluster
+  mpg_security_group_name = "mpg_security_group" # Name of the security group for the PostgreSQL cluster
+  pg_cluster_name         = "mpg-cluster"        # Name of the PostgreSQL cluster
+  pg_username             = "pg-user"            # Username of the PostgreSQL cluster
+  pg_db_name              = "db1"                # Name of the PostgreSQL database
+
+  # Managed Service for Apache Kafka®:
+  mkf_network_name        = "mkf_network"        # Name of the network for the Apache Kafka® cluster
+  mkf_subnet_name         = "mkf_subnet-a"       # Name of the subnet for the Apache Kafka® cluster
+  mkf_security_group_name = "mkf_security_group" # Name of the security group for the Apache Kafka® cluster
+  kf_cluster_name         = "mkf-cluster"        # Name of the Apache Kafka® cluster
+  kf_topic                = "sensors"            # Name of the Apache Kafka® topic
+  kf_username             = "mkf-user"           # Username of the Apache Kafka® cluster
+
+  # Data Transfer:
+  target_endpoint_name    = "pg-target-tf"     # Name of the target endpoint for the Managed Service for PostgreSQL
+  transfer_name           = "mkf-mpg-transfer" # Name of the transfer from the Managed Service for Apache Kafka® to the Managed Service for PostgreSQL
 }
+
+# Network infrastructure
 
 resource "yandex_vpc_network" "mpg_network" {
   description = "Network for Managed Service for PostgreSQL"
-  name        = "mpg_network"
+  name        = local.mpg_network_name
 }
 
 resource "yandex_vpc_network" "mkf_network" {
   description = "Network for Managed Service for Apache Kafka®"
-  name        = "mkf_network"
+  name        = local.mkf_network_name
 }
 
 resource "yandex_vpc_subnet" "mpg_subnet-a" {
-  description    = "Subnet in ru-central1-a availability zone for PostgreSQL"
-  name           = "mpg_subnet-a"
+  description    = "Subnet in the ru-central1-a availability zone for PostgreSQL"
+  name           = local.mpg_subnet_name
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.mpg_network.id
   v4_cidr_blocks = ["10.128.0.0/18"]
 }
 
 resource "yandex_vpc_subnet" "mkf_subnet-a" {
-  description    = "Subnet ru-central1-a availability zone for Apache Kafka®"
-  name           = "mkf_subnet-a"
+  description    = "Subnet in the ru-central1-a availability zone for Apache Kafka®"
+  name           = local.mkf_subnet_name
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.mkf_network.id
   v4_cidr_blocks = ["10.129.0.0/24"]
@@ -45,12 +70,12 @@ resource "yandex_vpc_subnet" "mkf_subnet-a" {
 resource "yandex_vpc_security_group" "mpg_security_group" {
   description = "Security group for Managed Service for PostgreSQL"
   network_id  = yandex_vpc_network.mpg_network.id
-  name        = "Managed PostgreSQL security group"
+  name        = local.mpg_security_group_name
 
   ingress {
     description    = "Allow incoming traffic from the Internet"
     protocol       = "TCP"
-    port      = 6432
+    port           = 6432
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -66,7 +91,7 @@ resource "yandex_vpc_security_group" "mpg_security_group" {
 resource "yandex_vpc_security_group" "mkf_security_group" {
   description = "Security group for Managed Service for Apache Kafka®"
   network_id  = yandex_vpc_network.mkf_network.id
-  name        = "Managed Apache Kafka® security group"
+  name        = local.mkf_security_group_name
 
   ingress {
     description    = "Allow incoming traffic from the port 9091"
@@ -84,9 +109,11 @@ resource "yandex_vpc_security_group" "mkf_security_group" {
   }
 }
 
+# Infrastructure for the Managed Service for PostgreSQL cluster
+
 resource "yandex_mdb_postgresql_cluster" "mpg-cluster" {
   description        = "Managed Service for PostgreSQL cluster"
-  name               = "mpg-cluster"
+  name               = local.pg_cluster_name
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.mpg_network.id
   security_group_ids = [yandex_vpc_security_group.mpg_security_group.id]
@@ -107,25 +134,29 @@ resource "yandex_mdb_postgresql_cluster" "mpg-cluster" {
   }
 }
 
+# User of the Managed service for the PostgreSQL cluster
 resource "yandex_mdb_postgresql_user" "pg-user" {
   cluster_id = yandex_mdb_postgresql_cluster.mpg-cluster.id
-  name       = "pg-user"
+  name       = local.pg_username
   password   = local.pg_password
 }
 
+# Database of the Managed service for the PostgreSQL cluster
 resource "yandex_mdb_postgresql_database" "mpg-db" {
   cluster_id = yandex_mdb_postgresql_cluster.mpg-cluster.id
-  name       = "db1"
+  name       = local.pg_db_name
   owner      = yandex_mdb_postgresql_user.pg-user.name
   depends_on = [
     yandex_mdb_postgresql_user.pg-user
   ]
 }
 
+# Infrastructure for the Managed Service for Apache Kafka® cluster
+
 resource "yandex_mdb_kafka_cluster" "mkf-cluster" {
   description        = "Managed Service for Apache Kafka® cluster"
   environment        = "PRODUCTION"
-  name               = "mkf-cluster"
+  name               = local.kf_cluster_name
   network_id         = yandex_vpc_network.mkf_network.id
   security_group_ids = [yandex_vpc_security_group.mkf_security_group.id]
 
@@ -144,39 +175,47 @@ resource "yandex_mdb_kafka_cluster" "mkf-cluster" {
     zones = ["ru-central1-a"]
   }
 
-  user {
-    name     = "mkf-user"
-    password = local.kf_password
-    permission {
-      topic_name = "sensors"
-      role       = "ACCESS_ROLE_CONSUMER"
-    }
-    permission {
-      topic_name = "sensors"
-      role       = "ACCESS_ROLE_PRODUCER"
-    }
-  }
+  depends_on = [
+    yandex_vpc_subnet.mkf_subnet-a
+  ]
 }
 
-# Managed Service for Apache Kafka® topic
+# Topic of the Managed Service for Apache Kafka® cluster
 resource "yandex_mdb_kafka_topic" "sensors" {
   cluster_id         = yandex_mdb_kafka_cluster.mkf-cluster.id
-  name               = "sensors"
+  name               = local.kf_topic
   partitions         = 1
   replication_factor = 1
 }
 
+# User of the Managed Service for Apache Kafka® cluster
+resource "yandex_mdb_kafka_user" "mkf-user" {
+  cluster_id = yandex_mdb_kafka_cluster.mkf-cluster.id
+  name       = local.kf_username
+  password   = local.kf_password
+  permission {
+    topic_name = yandex_mdb_kafka_topic.sensors.name
+    role       = "ACCESS_ROLE_CONSUMER"
+  }
+  permission {
+    topic_name = yandex_mdb_kafka_topic.sensors.name
+    role       = "ACCESS_ROLE_PRODUCER"
+  }
+}
+
+# Data Transfer infrastructure
+
 resource "yandex_datatransfer_endpoint" "pg_target" {
+  description = "Target endpoint for the Managed Service for PostgreSQL cluster"
   count       = local.transfer_enabled
-  description = "Target endpoint for PostgreSQL cluster"
-  name = "pg-target-tf"
+  name        = local.target_endpoint_name
   settings {
     postgres_target {
       connection {
         mdb_cluster_id = yandex_mdb_postgresql_cluster.mpg-cluster.id
       }
       database = yandex_mdb_postgresql_database.mpg-db.name
-      user = yandex_mdb_postgresql_user.pg-user.name
+      user     = yandex_mdb_postgresql_user.pg-user.name
       password {
         raw = local.pg_password
       }
@@ -185,9 +224,9 @@ resource "yandex_datatransfer_endpoint" "pg_target" {
 }
 
 resource "yandex_datatransfer_transfer" "mkf-mpg-transfer" {
-  count       = local.transfer_enabled
   description = "Transfer from the Managed Service for Apache Kafka® to the Managed Service for PostgreSQL"
-  name        = "mkf-mpg-transfer"
+  count       = local.transfer_enabled
+  name        = local.transfer_name
   source_id   = local.kf_source_endpoint_id
   target_id   = yandex_datatransfer_endpoint.pg_target[count.index].id
   type        = "INCREMENT_ONLY" # Data replication from the source Managed Service for Apache Kafka® topic to the target Managed Service for PostgreSQL cluster
